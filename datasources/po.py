5c3b8980d504dbc3a7b82fa4acc957db08a77d53
Murilo Matsubara <murilo.matsubara@gmail.com>
2023-11-30 15:18:36 -0300

    refactor: use to_schema to map po data

--- datasources/po.py
+++ datasources/po.py
@@ -1,6 +1,7 @@
 import polars as pl
 import datasus
 import ftp
+from pl_utils import to_schema, Column, DateColumn
 
 MAIN_TABLE = "PO_PAINEL_ONCOLOGIA"
 
@@ -21,42 +22,39 @@ def fetch_po(ftp_path: str):
 
 
 def map_po(df: pl.DataFrame):
-    return (
-        df.with_columns(
-            [
-                pl.when(pl.col(pl.Utf8).str.len_chars() == 0)
-                .then(None)
-                .otherwise(pl.col(pl.Utf8))
-                .name.keep(),
-            ]
-        )
-        .with_columns(
-            [
-                pl.when(pl.col("IDADE").cast(pl.UInt16).gt(200))
-                .then(None)
-                .otherwise(pl.col("IDADE"))
-                .name.keep(),
-            ]
-        )
-        .with_columns(
-            [
-                pl.col("ANO_DIAGN").cast(pl.UInt16),
-                pl.col("ANO_TRATAM").cast(pl.UInt16),
-                pl.col("UF_RESID").cast(pl.UInt8),
-                pl.col("MUN_RESID").cast(pl.UInt32),
-                pl.col("UF_TRATAM").cast(pl.UInt8),
-                pl.col("MUN_TRATAM").cast(pl.UInt32),
-                pl.col("UF_DIAGN").cast(pl.UInt8),
-                pl.col("MUN_DIAG").cast(pl.UInt32),
-                pl.col("TRATAMENTO").cast(pl.UInt8),
-                pl.col("DIAGNOSTIC").cast(pl.UInt8),
-                pl.col("IDADE").cast(pl.UInt8),
-                pl.col("ESTADIAM").cast(pl.UInt8),
-                pl.col("CNES_DIAG").cast(pl.UInt32),
-                pl.col("CNES_TRAT").cast(pl.UInt32),
-                pl.col("DT_DIAG").str.to_date(),
-                pl.col("DT_TRAT").str.to_date(),
-                pl.col("DT_NASC").str.to_date(),
-            ]
-        )
+    df = df.with_columns(
+        [
+            pl.when(pl.col(pl.Utf8).str.len_chars() == 0)
+            .then(None)
+            .otherwise(pl.col(pl.Utf8))
+            .name.keep(),
+        ]
+    )
+    return to_schema(
+        df,
+        [
+            Column("ANO_DIAGN", pl.UInt16),
+            Column("ANOMES_DIA", pl.UInt32),
+            Column("ANO_TRATAM", pl.UInt16),
+            Column("ANOMES_TRA", pl.UInt32),
+            Column("UF_RESID", pl.UInt8),
+            Column("MUN_RESID", pl.UInt32),
+            Column("UF_TRATAM", pl.UInt8),
+            Column("MUN_TRATAM", pl.UInt32),
+            Column("UF_DIAGN", pl.UInt8),
+            Column("MUN_DIAG", pl.UInt32),
+            Column("TRATAMENTO", pl.UInt8),
+            Column("DIAGNOSTIC", pl.UInt8),
+            Column("IDADE", pl.UInt8, strict=False),
+            Column("SEXO", pl.Utf8),
+            Column("ESTADIAM", pl.UInt8),
+            Column("CNES_DIAG", pl.UInt32),
+            Column("CNES_TRAT", pl.UInt32),
+            Column("TEMPO_TRAT", pl.Utf8),
+            Column("CNS_PAC", pl.Utf8),
+            Column("DIAG_DETH", pl.Utf8),
+            DateColumn("DT_DIAG", "%d/%m/%Y"),
+            DateColumn("DT_TRAT", "%d/%m/%Y"),
+            DateColumn("DT_NASC", "%d/%m/%Y"),
+        ],
     )
